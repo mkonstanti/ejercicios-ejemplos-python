11a6d93c3d6db16f4f3a4f7a4fbe5265deb9b489
mkonstanti <71271552+mkonstanti@users.noreply.github.com>
2025-02-15 10:48:14 +0100

    tema 2 ejercicio 1 realizado

---
+++ ejercicios/t2ejercicio1peliculas.py
@@ -0,0 +1,40 @@
+#Declara variables para guardar la siguiente información sobre tu película favorita y muéstrala en la consola. Debes declarar:
+
+#Título de la película
+titulo = "¡Olvídate de mí!"
+#Director de la película
+director = "Michel Gondry"
+#Año de lanzamiento
+año = 2004
+#Género
+genero = "Comedia romántica"
+#Duración en minutos
+duracion = 108
+#¿Tiene premios? (verdadero o falso)
+tiene_premios = True
+
+#Imprimir las variables 
+print("Mi película favorita es:")
+print("Título de la película:", titulo)
+print("Director de la película:", director)
+print("Año de lanzamiento:", año)
+print("Género:", genero)
+print("Duración en minutos:", duracion)
+print("¿Tiene premios?", tiene_premios)
+
+#Luego, cambia el valor de las variables a datos en inglés y vuelve a mostrarlas en la consola.
+
+titulo = "Eternal sunshine of the spotless mind "
+director = "Michel Gondry"
+año = 2004
+genero = "Romantic comedy"
+duracion = 108
+tiene_premios = True
+
+print("My favorite movie is :")
+print("Movie title:", titulo)
+print("Director of the movie:", director)
+print("Year:", año)
+print("Genre:", genero)
+print("Duration in minutes:", duracion)
+print("Does it have any awards?", tiene_premios)
