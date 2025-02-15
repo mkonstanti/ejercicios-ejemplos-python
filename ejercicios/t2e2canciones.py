1b4275fb101baffc68ca2c0a52d4c50ccdb27c54
mkonstanti <71271552+mkonstanti@users.noreply.github.com>
2025-02-15 11:21:00 +0100

    tema 2 ejercicio 2 realizado

---
+++ ejercicios/t2e2canciones.py
@@ -0,0 +1,33 @@
+#Pedir al usuario que introduca la siguiente información sobre su canción favorita
+
+print("\nIntroduce los datos de tu canción que menos te gusta: ")
+titulo = input("Cual es el título de tu canción favorita? ")
+artista = input("Cómo se llama el artista? ")
+album = input("Cual es el album de la canción? ")
+año = input("En que año se lanzó la canción? ")
+duracion = input("Cual es la duración de la canción en segundos? ")
+tiene_videoclip = input("Tiene videoclip la canción? (True/False): ")
+
+print("Tu canción favorita es", titulo)
+print("El artista de la canción se llama", artista)
+print("El album de la canción es", album)
+print("La canción se lanzó en el año", año)
+print("La duración de la canción en segundos es", duracion)
+print("Tiene videoclip la canción?", tiene_videoclip)
+
+#Pedir al usuario que introduca la siguiente información sobre la canción que menos le guste
+
+print("\nIntroduce los datos de la canción que menos te gusta: ")
+titulo = input("Cual es su título de la canción que menos te gusta? ")
+artista = input("Cómo se llama el artista? ")
+album = input("Cual es el album de la canción? ")
+año = input("En que año se lanzó la canción? ")
+duracion = input("Cual es la duración de la canción en segundos? ")
+tiene_videoclip = input("Tiene videoclip la canción? (True/False): ")
+
+print("La canción que menos te gusta es", titulo)
+print("El artista de la canción se llama", artista)
+print("El album de la canción es", album)
+print("La canción se lanzó en el año", año)
+print("la duración de la canción en segundos es", duracion)
+print("Tiene videoclip la canción?", tiene_videoclip)
