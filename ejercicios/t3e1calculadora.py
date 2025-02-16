aece3e9ed6489bee71bc5fe3591fc2d9da35dbfb
mkonstanti <71271552+mkonstanti@users.noreply.github.com>
2025-02-16 17:57:41 +0100

    tema 3 ejercicio 1 realizado

---
+++ ejercicios/t3e1calculadora.py
@@ -0,0 +1,15 @@
+#Pedir al usuario que introduzca los numeros
+numero1 = float(input("Introduzca un numero: "))
+numero2 = float(input("introduzca otro numero: "))
+
+#Hacer la operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+#Mostrar los resultados
+print("EL resultado de la suma de los numeros es: ", suma)
+print("EL resultado de la resta de los numeros es: ", resta)
+print("El resultado de la multiplicación de los dos numeros es: ", multiplicacion)
+print("El resultado de la division de los dos numeros es: ", division)
